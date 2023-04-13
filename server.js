ed688c3d7b75e145976cef08e4f17b09e5bd2558
Mathis Kazek <>
2023-04-13 09:27:32 +0200

    Fix var

--- server.js
+++ server.js
@@ -11,18 +11,14 @@ const connectDatabase = require('./config/database');
 
 const port = process.env.PORT || 8000;
 
-const auth = require('./routes/leaderboard');
+const leaderboard = require('./routes/leaderboard');
 
 app.use(cors());
 app.use(express.json());
 
 connectDatabase();
 
-app.use('/api/v1', auth);
-
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, './build', 'index.html'));
-});
+app.use('/api/v1', leaderboard);
 
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
